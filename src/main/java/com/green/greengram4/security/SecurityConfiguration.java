05bcd08c9ccbb12875ce8e319a37e60be65b4470
sunxno <sunxno@naver.com>
2024-02-13 14:43:35 +0900

    security

--- src/main/java/com/green/greengram4/security/SecurityConfiguration.java
+++ src/main/java/com/green/greengram4/security/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.green.greengram4.security;
 
+import com.green.greengram4.security.auth2.CustomOAuth2UserService;
+import com.green.greengram4.security.auth2.OAuth2AuthenticationFailureHandler;
+import com.green.greengram4.security.auth2.OAuth2AuthenticationRequestBasedOnCookieRepository;
+import com.green.greengram4.security.auth2.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +20,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandlerll;
+    private final OAuth2AuthenticationRequestBasedOnCookieRepository oAuth2AuthenticationRequestBasedOnCookieRepository;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,24 +32,32 @@ public class SecurityConfiguration {
                 .formLogin(form -> form.disable())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers(
-                                          "/api/feed"
+                                        "/api/feed"
                                         , "/api/feed/comment"
                                         , "/api/dm"
                                         , "/api/dm/msg"
-                                        ).authenticated()
-                                        .requestMatchers(HttpMethod.POST, "/api/user/signout"
-                                                                        , "/api/user/follow"
-                                        ).authenticated()
-                                        .requestMatchers(HttpMethod.GET, "/api/user").authenticated()
-                                        .requestMatchers(HttpMethod.PATCH, "/api/user/pic").authenticated()
-                                        .requestMatchers(HttpMethod.GET, "/api/feed/fav").hasAnyRole("ADMIN")
-                                        .anyRequest().permitAll()
+                                ).authenticated()
+                                .requestMatchers(HttpMethod.POST, "/api/user/signout"
+                                        , "/api/user/follow"
+                                ).authenticated()
+                                .requestMatchers(HttpMethod.GET, "/api/user").authenticated()
+                                .requestMatchers(HttpMethod.PATCH, "/api/user/pic").authenticated()
+                                .requestMatchers(HttpMethod.GET, "/api/feed/fav").hasAnyRole("ADMIN")
+                                .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(except -> {
                     except.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                             .accessDeniedHandler(new JwtAccessDeniedHandler());
                 })
+                .oauth2Login(oauth2 -> oauth2.authorizationEndpoint(auth ->
+                                        auth.baseUri("/oauth2/authorization")
+                                                .authorizationRequestRepository(oAuth2AuthenticationRequestBasedOnCookieRepository)
+                                ).redirectionEndpoint(redirection -> redirection.baseUri("/*/oauth2/code/*"))
+                                .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                .failureHandler(oAuth2AuthenticationFailureHandlerll)
+                )
                 .build();
     }
 
